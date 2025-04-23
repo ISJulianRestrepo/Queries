8f577aafb675e47b71eb8f212b17e54943026ba5
ISJulianRestrepo <jrestrepob@siesa.com>
2025-04-23 08:01:15 -0500

    ajustes querys Vatia

--- Vatia/inventario_a_la_fecha.sql
+++ Vatia/inventario_a_la_fecha.sql
@@ -1,12 +1,19 @@
-SELECT f120_id                                                                                               f_item,
-    f120_descripcion                                                                                      f_desc_item,
-    f417_id                                                                                               f_id,
+
+DECLARE @p_fecha_desde AS datetime = {'FechaInicio'}--'2023-08-01'
+-- DECLARE @p_fecha_desde AS datetime = '2023-08-01'
+DECLARE @p_fecha_hasta AS datetime = {'FechaFin'}--'2023-08-23'
+-- DECLARE @p_fecha_hasta AS datetime = '2025-08-23'
+
+
+SELECT f120_id                                                                                            Item,
+    f120_descripcion                                                                                      DescItem,
+    f417_id                                                                                               Serial,
     --Rtrim(f2_350_id_co) + '-' + Rtrim(f2_350_id_tipo_docto) + '-' + RIGHT('00000000' + Rtrim(f2_350_consec_docto), 8) f_docto_salida,
-    CAST(f417_fecha_salida AS DATE)                                                                 f_fecha_salida,
-    f150_id                                                                                               f_id_bodega,
-    f417_id_ubicacion                                                                                     f_id_ubicacion,
-    f417_id_lote                                                                                          f_id_lote,
-    f120_id_unidad_inventario                                                                             f_um,
+    CAST(f417_fecha_salida AS DATE)                                                                       FechaSalida,
+    f150_id                                                                                               Bodega,
+    f417_id_ubicacion                                                                                     Ubicacion,
+    f417_id_lote                                                                                          Lote,
+    f120_id_unidad_inventario                                                                             UM,
     CASE
                       WHEN v417_estado = 1 THEN 1
                       ELSE 0
@@ -19,20 +26,17 @@ SELECT f120_id                                                                  
                       WHEN 2 THEN 'No disponible'
                       WHEN 1 THEN 'Disponible'
                       WHEN -1 THEN 'Creado'
-           END                                                                                                         f_ind_estado,
-    f120_referencia                                                                                             f_referencia,
-    Rtrim(f420_id_tipo_docto) + '-' + RIGHT('00000000' + Rtrim(f420_consec_docto), 8)                                       f_oc_docto,
-    CAST(f417_fecha_creacion AS DATE)                                                                     f_fecha_creacion,
-    CAST(f417_fecha_ingreso AS DATE)                                                                      f_fecha_ingreso,
-    CAST(t470_2.f_fecha_ultimo_ingreso AS DATE)                                                           f_fecha_ultimo_ingreso,
-    f150_descripcion                                                                                            f_desc_bodega,
-    f120_descripcion_corta                                                                                      f_desc_corta,
-    f417_rowid                                                                                                  f_rowid,
-    f417_rowid_item_ext                                                                                         f_rowid_item_ext
+           END                                                                                                         Estado,
+    f120_referencia                                                                                             Referencia,
+    Rtrim(f420_id_tipo_docto) + '-' + RIGHT('00000000' + Rtrim(f420_consec_docto), 8)                                       OrdenDeCompra,
+    CAST(f417_fecha_creacion AS DATE)                                                                     FechaCreacion,
+    CAST(f417_fecha_ingreso AS DATE)                                                                      FechaIngreso,
+    CAST(t470_2.f_fecha_ultimo_ingreso AS DATE)                                                           FechaUltimoIngreso,
+    f150_descripcion                                                                                            DecBodega
 FROM t417_cm_seriales
     LEFT JOIN
     (
-                                                      SELECT f417_rowid_item_ext v417_rowid_item,
+                                                                                      SELECT f417_rowid_item_ext v417_rowid_item,
             f417_rowid          v417_rowid,
             CASE Sum(CASE f470_ind_naturaleza
                         WHEN 1 THEN 1
@@ -50,7 +54,6 @@ FROM t417_cm_seriales
             ON         f121_rowid = f417_rowid_item_ext
             INNER JOIN t120_mc_items
             ON         f120_rowid = f121_rowid_item
-        WHERE      f470_id_fecha <= 'Apr 14 2025 12:00AM'
         GROUP BY   f417_rowid_item_ext,
                                  f417_rowid
     UNION ALL
@@ -72,7 +75,6 @@ FROM t417_cm_seriales
             ON         f121_rowid = f417_rowid_item_ext
             INNER JOIN t120_mc_items
             ON         f120_rowid = f121_rowid_item
-        WHERE      f470_id_fecha <= 'Apr 14 2025 12:00AM'
         GROUP BY   f417_rowid_item_ext,
                                  f417_rowid)v417
     ON         v417.v417_rowid = f417_rowid
@@ -95,7 +97,7 @@ FROM t417_cm_seriales
             Min(f1_470_rowid_docto)    f1_470_rowid_docto,
             Min(f1_470_rowid_op_docto) f1_470_rowid_op_docto
         FROM (
-                                                                                                                              SELECT f1_rowid_serial,
+                                                                                                                                                                                              SELECT f1_rowid_serial,
                     f470_rowid_docto    f1_470_rowid_docto,
                     f470_rowid_op_docto f1_470_rowid_op_docto
                 FROM (
@@ -120,7 +122,6 @@ FROM t417_cm_seriales
                                     ELSE 2
                                     END, 
                                     0) IN (0,1)
-                        AND CAST(f417_fecha_creacion AS DATE) <= 'Apr 14 2025 12:00AM'
                         AND f417_id_cfg_serial = '01'
                         AND f470_ind_naturaleza = 1
                     GROUP BY   f479_rowid_serial ) tbl1
@@ -151,7 +152,6 @@ FROM t417_cm_seriales
                                                                                                           WHEN 1 THEN 1
                                                                                                           ELSE 2
                                                                                                END, 0) IN (0,1)
-                        AND CAST(f417_fecha_creacion AS DATE) <= 'Apr 14 2025 12:00AM'
                         AND f417_id_cfg_serial = '01'
                         AND f470_ind_naturaleza = 1
                     GROUP BY   f479_rowid_serial ) tbl1
@@ -167,7 +167,7 @@ FROM t417_cm_seriales
     ON         f420_rowid = f1_451_rowid_oc_docto
     LEFT JOIN
     (
-                                                      SELECT f479_rowid_serial   f_rowid_serial,
+                                                                                      SELECT f479_rowid_serial   f_rowid_serial,
             f470_rowid_item_ext f470_rowid_item_ext ,
             Max(f470_id_fecha)  f_fecha_ultimo_ingreso
         FROM t479_cm_movto_seriales
@@ -191,8 +191,8 @@ FROM t417_cm_seriales
     ON         t470_2.f470_rowid_item_ext = f417_rowid_item_ext
         AND t470_2.f_rowid_serial =f417_rowid
         AND Isnull(v417_estado, 0) IN (0,1)
-        AND CAST(f417_fecha_creacion AS DATE) <= 'Apr 14 2025 12:00AM'
         AND f417_id_cfg_serial = '01'
 
 
 WHERE v417_estado IN (1)
+    AND f417_fecha_creacion BETWEEN @p_fecha_desde AND @p_fecha_hasta
