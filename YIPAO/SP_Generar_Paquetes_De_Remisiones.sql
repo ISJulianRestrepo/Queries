fa6a48e489541ebefeff7389860e772a8b4514e8
ISJulianRestrepo <jrestrepob@siesa.com>
2025-06-05 09:22:32 -0500

    Generar paquetes de remisiones V2

--- YIPAO/SP_Generar_Paquetes_De_Remisiones.sql
+++ YIPAO/SP_Generar_Paquetes_De_Remisiones.sql
@@ -11,22 +11,35 @@ BEGIN
     -- CTE para calcular RowNum
     WITH CTE AS (
         SELECT 
-            JSON_VALUE(jsonOrigen, '$.transactionId') AS Id, 
-            ROW_NUMBER() OVER (ORDER BY JSON_VALUE(jsonOrigen, '$.transactionType'), JSON_VALUE(jsonOrigen, '$.dispenserName')) AS RowNum
+            transacciones AS Id, 
+            ROW_NUMBER() OVER (ORDER BY transactionType, dispenserName) AS RowNum
             -- ROW_NUMBER() OVER (ORDER BY JSON_VALUE(jsonOrigen, '$.transactionType')) AS RowNum
         FROM 
-            remisiones
-        WHERE paquete IS NULL
+            (
+                SELECT JSON_VALUE(jsonOrigen, '$.productName')  AS productName, 
+                JSON_VALUE(jsonOrigen, '$.transactionType')     AS transactionType, 
+                JSON_VALUE(jsonOrigen, '$.dispenserName')       AS dispenserName,
+                COUNT(*)                                        AS Total,
+                STRING_AGG(CAST(JSON_VALUE(jsonOrigen, '$.transactionId') AS VARCHAR), ', ') AS transacciones
+                FROM remisiones
+                WHERE paquete IS NULL
+                GROUP BY 
+                    JSON_VALUE(jsonOrigen, '$.dispenserName'),
+                    JSON_VALUE(jsonOrigen, '$.productName'),  
+                    JSON_VALUE(jsonOrigen, '$.transactionType')                  
+            )AS DatosAEmpaquetar        
     )
+
+
     
     -- Actualiza el campo paquete
     UPDATE T
     SET T.paquete = CONCAT(
-        CONVERT(VARCHAR(8), REPLACE(JSON_VALUE(jsonOrigen, '$.date'), '-', ''), 112),
-        ((CTE.RowNum - 1) / 1000) + 1
-    )
+                        CONVERT(VARCHAR(8), REPLACE(JSON_VALUE(jsonOrigen, '$.date'), '-', ''), 112),
+                        ((CTE.RowNum - 1) / 500) + 1
+                    )
     FROM remisiones T
-    JOIN CTE ON JSON_VALUE(T.jsonOrigen, '$.transactionId') = CTE.Id
+    JOIN CTE ON  CTE.Id  LIKE '%' + JSON_VALUE(T.jsonOrigen, '$.transactionId') + '%'  
     WHERE T.paquete IS NULL;
 
     -- Retorna los datos seleccionados
@@ -41,3 +54,4 @@ BEGIN
     ORDER BY CONVERT(bigint,paquete) DESC;
 END;
 GO
+
